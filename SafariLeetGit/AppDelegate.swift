d77a36f31e72e2563d3ebc26046cb274b63bbcc0
Devansh Pratap Singh <myivy0412@gmail.com>
2026-07-15 23:37:00 +0530

    enchancements

--- SafariLeetGit/AppDelegate.swift
+++ SafariLeetGit/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    
+    
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
